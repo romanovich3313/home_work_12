269b75f36ac1b11720c66d8ec0e2d0593da02448
romanovich3313 <124934750+romanovich3313@users.noreply.github.com>
2023-05-17 20:46:26 +0300

    create

---
+++ bot.py
@@ -0,0 +1,105 @@
+from classes import Name, Phone, AddressBook, Record
+
+USERS = AddressBook()
+
+
+def error_handler(func):
+    def inner(*args):
+        try:
+            result = func(*args)
+            return result
+        except KeyError:
+            return "No user"
+        except ValueError:
+            return 'Give me name and phone please'
+        except IndexError:
+            return 'Enter user name'
+
+    return inner
+
+
+def hello_user():
+    return "How can I help you?"
+
+
+@error_handler
+def add_contact(name, phone):
+    name = Name(name)
+    phone = Phone(phone)
+    record = Record(name, phone)
+    result = USERS.add_record(record)
+    return f"User {result} added"
+
+
+@error_handler
+def change_phone(name, new_phone):
+    if name in USERS:
+        old_phone = USERS[name]
+        USERS[name] = new_phone
+        return f'User {name} has new phone number: {new_phone}, old phone number: {old_phone}'
+    else:
+        return f'This user: {name} is not in your phone book'
+
+
+@error_handler
+def show_phone(name):
+    return f'Phone number for {name}: {USERS[name]}'
+
+
+def show_all():
+    result = ''
+    for name, phone in USERS.items():
+        result += f'Name: {name}, phone: {phone}\n'
+    return result
+
+
+def unknown_command(command):
+    return f"Not command {command}"
+
+
+def close_bot():
+    exit("Good bye")
+
+
+HANDLERS = {
+    'add': add_contact,
+    'phone': show_phone,
+    'all': show_all,
+    'change': change_phone,
+    'exit': close_bot,
+    'goodbye': close_bot,
+    'close': close_bot,
+}
+
+
+def parse_command(command):
+    parts = command.split()
+    command_name = parts[0]
+    if len(parts) > 1:
+        command_args = parts[1:]
+    else:
+        command_args = []
+    return command_name, command_args
+
+
+def main():
+    
+    book = AddressBook()
+    book.read_from_file('phone_book.bin')
+
+    print(hello_user())
+    while True:
+        command_line = input('Please enter command and args: ').strip().lower()
+        command_name, command_args = parse_command(command_line)
+        if command_name in HANDLERS:
+            try:
+                result = HANDLERS[command_name](*command_args)
+                print(result)
+            except TypeError:
+                print('Invalid input. Please try again.')
+        else:
+            print(unknown_command(command_name))
+
+
+if __name__ == '__main__':
+    main()
---
+++ classes.py
@@ -0,0 +1,158 @@
+from collections import UserDict
+from datetime import datetime, date
+import re
+import pickle
+
+
+class AddressBook(UserDict):
+    def add_record(self, record):
+        self.data[record.name.value] = record
+
+    def iterator(self, it):
+        records = list(self.data.values())
+        for i in range(0, len(records), it):
+            yield records[i : i + it]
+
+    def save_to_file(self, filename):
+        with open(filename, 'wb') as file:
+            pickle.dump(self.data, file)
+
+    def read_from_file(self, filename):
+        with open(filename, "rb") as file:
+            data = pickle.load(file)
+        return data
+
+    def search(self, keyword):
+        results = []
+        keyword = keyword.lower()
+        for record in self.data.values():
+            if keyword in record.name.value.lower():
+                results.append(record)
+            for phone in record.phones:
+                if keyword in phone.value.lower():
+                    results.append(record)
+                    break
+        return results
+
+class Record:
+    def __init__(self, name, phone, birthday=None):
+        self.name = Name(name)
+        self.birthday = Birthday(birthday) if birthday else None
+        self.phones = []
+        if phone is not None:
+            self.phones.append(phone)
+
+    @property
+    def phones(self):
+        return self.__phones
+
+    @phones.setter
+    def phones(self, phones):
+        self.__phones = phones
+
+    @property
+    def birthday(self):
+        return self.__birthday
+
+    @birthday.setter
+    def birthday(self, birthday):
+        self.__birthday = birthday
+
+    def add_phone(self, phone):
+        self.phones.append(phone)
+
+    def remove_phone(self, phone):
+        self.phones.remove(phone)
+
+    def change_phone(self, old_phone, new_phone):
+        old_index = self.phones.index(old_phone)
+        self.phones[old_index] = new_phone
+
+    def days_to_birthday(self):
+        if not self.birthday:
+            return None
+        today = date.today()
+        next_birthday = self.birthday.value.replace(year=today.year)
+        if next_birthday < today:
+            next_birthday = next_birthday.replace(year=today.year + 1)
+        days_to_birthday = (next_birthday - today).days
+        return days_to_birthday
+
+
+class Field:
+    def __init__(self, value):
+        self.value = value
+
+    @property
+    def value(self):
+        return self._value
+
+    @value.setter
+    def value(self, value):
+        self._value = value
+
+
+class Name(Field):
+    def __init__(self, name: str):
+        super().__init__(name)
+
+
+class Phone(Field):
+    def __init__(self, phone):
+        self.value = phone
+
+    @Field.value.setter
+    def value(self, phone):
+        if not isinstance(phone, str) or len(phone) != 10:
+            raise ValueError("Invalid phone number")
+        self._value = phone[:3] + "-" + phone[3:6] + "-" + phone[6:]
+
+
+class Birthday(Field):
+    def __init__(self, birthday):
+        # self._value = None
+        self.value = birthday
+
+    @Field.value.setter
+    def value(self, birthday):
+        try:
+            dt = datetime.strptime(birthday, "%d.%m.%Y")
+        except (ValueError, TypeError):
+            raise Exception("Invalid birthday. Only string format dd.mm.yyyy")
+        Field.value.fset(self, dt.date())
+
+
+book = AddressBook()
+
+
+record = Record("roma", "10.12.1995")
+record2 = Record("roma", "20.12.1995")
+print(record.birthday)  # none!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+record.add_phone("23343434")
+book.add_record(record)
+print(record)
+
+name = Name("Ivan")
+phone = Phone("0958887481")
+birthday = Birthday("20.05.95")
+
+rec = Record(name, phone, birthday)
+book.add_record(rec)
+print(rec.days_to_birthday())
+
+# record = Record("roma", "10-12-1995")
+# record.add_phone("23343434")
+# record.add_phone("23343434")
+# record.add_phone("49586548")
+
+# book.add_record(record)
+# print(record)
+
+# name = Name("Ivan")
+# phone = Phone("095-888-7481")
+# birthday = Birthday("20-05-95")
+
+# rec = Record(name, phone, birthday)
+# book.add_record(rec)
+# print(rec.days_to_birthday())
